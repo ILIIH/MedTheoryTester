53566020958b948bbc024a2fc9e3ba6f1e76ef82
illiabranchuk <jointlord0@gmail.com>
2024-09-25 19:01:19 +0100

    fix

--- app/src/main/java/com/example/medtheorytester/viewModel/QuizViewModel.kt
+++ app/src/main/java/com/example/medtheorytester/viewModel/QuizViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.medtheorytester.viewModel
 
+import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
@@ -8,35 +9,40 @@ import com.example.domain.model.Riddle
 import com.example.domain.useCase.GetAllRiddlesUseCase
 import kotlinx.coroutines.launch
 
-const val  BATCH_SIZE = 5;
+const val  BATCH_SIZE = 4;
 class QuizViewModel(
     private val getAllRiddlesUseCase: GetAllRiddlesUseCase
 ) : ViewModel() {
 
     private val _riddlesListState = mutableStateOf<List<Riddle>>(emptyList())
-    private var index = 0;
     private val _currentRiddle = mutableStateOf<Riddle?>(null)
     val currentRiddle: State<Riddle?> get() = _currentRiddle
 
     private val _isLoaded = mutableStateOf(false)
+
+    private var index = 0;
+    private var batch_limit = 0;
+
     val isLoaded: State<Boolean> get() = _isLoaded
 
     init {
-        fetchRiddles()
+        fetchRiddles(batch_limit)
     }
     fun next(){
         index++;
-        if(index%BATCH_SIZE != 0){
+        if(_riddlesListState.value.size > index){
             _currentRiddle.value = _riddlesListState.value[index];
         }
         else {
-            fetchRiddles(index)
+            fetchRiddles(batch_limit)
+            batch_limit += BATCH_SIZE
+            index = 0
         }
     }
-    fun fetchRiddles(index:Int  = 0 ) {
+    fun fetchRiddles(index:Int) {
         viewModelScope.launch {
             _isLoaded.value = true
-            _riddlesListState.value = getAllRiddlesUseCase.execute(0)
+            _riddlesListState.value = getAllRiddlesUseCase.execute(index)
             _currentRiddle.value = _riddlesListState.value.first();
             _isLoaded.value = false
         }
